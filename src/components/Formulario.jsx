6e576c89ffe4b8ac62ee39a12d68028840298f9f
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-19 18:26:18 -0500

    Validacion de formulario y componente error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import axios from "axios";
+import Error from "./Error";
 
 import useMoneda from "../hooks/useMoneda";
 import useCriptomoneda from "../hooks/useCriptomoneda";
@@ -8,6 +9,7 @@ import useCriptomoneda from "../hooks/useCriptomoneda";
 const Formulario = () => {
   //state del listado de criptomonedas
   const [listacripto, guardarCriptomonedas] = useState([]);
+  const [error, guardarError] = useState(false);
 
   const MONEDAS = [
     { codigo: "EUR", nombre: "Euro" },
@@ -41,8 +43,23 @@ const Formulario = () => {
     };
     consultarAPI();
   }, []);
+
+  //Uso del submit
+  const cotizarMoneda = (e) => {
+    e.preventDefault();
+
+    //validar si los campos estan llenos
+    if (moneda === "" || criptomoneda === "") {
+      guardarError(true);
+      return;
+    }
+
+    //pasar los datos al componente principal
+    guardarError(false);
+  };
   return (
-    <form>
+    <form onSubmit={cotizarMoneda}>
+      {error ? <Error mensaje="Todos los campos son obligatorios" /> : null}
       <SelectMonedas />
       <SelectCripto />
       <Boton type="submit" value="Calcular" />
